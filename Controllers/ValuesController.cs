96a975a3a16d7a3f5d0baf57494149831c6a7b33
burak.caglayan@outlook.com <burak.caglayan@outlook.com>
2019-01-08 21:25:20 +0300

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonPatchSample.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonPatchSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankAccountsController : ControllerBase
+    {
+        [HttpPatch("{bankAccountId}")]
+        public async Task<IActionResult> Patch(int bankAccountId, [FromBody]JsonPatchDocument<BankAccount> patches)
+        {
+            var bankAccount = new BankAccount()
+            {
+                Id = 1,
+                AccountName = "burak hesap",
+                Balance = 2000
+            };
+
+            patches.ApplyTo(bankAccount);
+
+            return Ok(bankAccount);
+
+        }
+    }
+}
---
+++ Models/BankAccount.cs
@@ -0,0 +1,9 @@
+namespace JsonPatchSample.Models
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public string AccountName { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
